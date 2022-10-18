42a649f7f38575284a5e3aaaed12d2e9b05d68b8
SofiaLoven <sofia.loven93@gmail.com>
2022-10-18 16:43:07 +0200

    Färdigt quiz för godkänd nivå

--- quiz.js
+++ quiz.js
@@ -15,7 +15,7 @@ let resultBtn = document.querySelector("#result");
 let result = document.querySelector("#colorResult");
 
 resultBtn.addEventListener("click", ()=>{
-result.innertext = "";
+result.innerHTML = "";
 
 let answer = document.querySelectorAll(" .q:checked");
 let answerArr = Array.from(answer);
@@ -24,10 +24,19 @@ filteredAnswer = answerArr.filter((value)=>{
     return value.value === "right";
 });
 
-let amount =filteredAnswer.length;
-let sum = `${amount}/10 poäng`;
+let amount = filteredAnswer.length;
+let sum = "";
 
+if(amount < 0.5*10){
+    result.style.color ="red";
+    sum = `${amount}/10 poäng. Du är tyvärr underkänd.`
+} else if (amount <= 0.75*10){
+    result.style.color ="orange";
+    sum =`${amount}/10 poäng. Du är godkänd.`
+}else{
+    result.style.color ="green";
+    sum = `${amount}/10 poäng. Du är mycket väl godkänd.`
+};
 result.append(sum);
-
 
 });
